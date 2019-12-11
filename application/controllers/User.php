48ceb9e28db8ba56f59ba539afcf8ba4c20b4cd9
yhaae01 <suryha7fold@gmail.com>
2019-12-11 21:00:46 +0700

    penambahan fitur ubah password

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -73,13 +73,54 @@ class User extends CI_Controller
 
      public function ubahPassword()
      {
-          $data['title'] = "Ubah Password";
+          $data['title'] = 'Ubah Password';
           $data['user']  = $this->ModelUser->getTopbarName();
 
-          $this->load->view('templates/header', $data);
-          $this->load->view('templates/sidebar', $data);
-          $this->load->view('templates/topbar', $data);
-          $this->load->view('user/ubah-password', $data);
-          $this->load->view('templates/footer');
+          $this->form_validation->set_rules('passwordlama', 'Password Lama', 'required|trim', [
+               'required' => "Password harus diisi!"
+          ]);
+
+          $this->form_validation->set_rules('passwordbaru1', 'Password Baru', 'required|trim|min_length[6]|matches[passwordbaru2]', [
+               'required' => 'Password baru harus diisi!',
+               'min_length' => 'Password minimal 6 karakter!',
+               'matches' => 'Password tidak sama!'
+          ]);
+
+          $this->form_validation->set_rules('passwordbaru2', 'Ulangi password baru', 'required|trim|min_length[6]|matches[passwordbaru1]', [
+               'required' => 'Password baru harus diisi!',
+               'min_length' => 'Password minimal 6 karakter!',
+               'matches' => 'Password tidak sama!'
+          ]);
+
+          if ($this->form_validation->run() == false) {
+               $this->load->view('templates/header', $data);
+               $this->load->view('templates/sidebar', $data);
+               $this->load->view('templates/topbar', $data);
+               $this->load->view('user/ubah-password', $data);
+               $this->load->view('templates/footer');
+          } else {
+               $passlama = $this->input->post('passwordlama');
+               $passbaru = $this->input->post('passwordbaru1');
+
+               if (!password_verify($passlama, $data['user']['password'])) {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password lama salah!</div>');
+                    redirect('user/ubahPassword');
+               } else {
+                    if ($passlama == $passbaru) {
+                         $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password tidak boleh sama!</div>');
+                         redirect('user/ubahPassword');
+                    } else {
+                         // Password ok
+                         $passhash = password_hash($passbaru, PASSWORD_DEFAULT);
+
+                         $this->db->set('password', $passhash);
+                         $this->db->where('email', $this->session->userdata('email'));
+                         $this->db->update('user');
+
+                         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Password berhasil diubah!</div>');
+                         redirect('user/ubahPassword');
+                    }
+               }
+          }
      }
 }
