a612808703c68a519f7c6fbff5512459b65f1a61
yhaae01 <suryha7fold@gmail.com>
2019-12-15 07:44:27 +0700

    menambahkan card di dashboard admin

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -13,6 +13,11 @@ class ModelUser extends CI_Model
           return $this->db->get_where('user', ['id' => $id])->row_array();
      }
 
+     public function getUserWhere($where = null)
+     {
+          return $this->db->get_where('user', $where);
+     }
+
      public function ubahUser()
      {
           $data = [
